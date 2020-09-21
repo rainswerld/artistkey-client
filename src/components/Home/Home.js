81adae495949da8a23a05126482b205e7a17ee1e
rainswerld <m.rainrobinson@gmail.com>
2020-09-21 13:54:57 -0600

    Update home page with signup/signin buttons

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Button } from 'react-bootstrap'
 
 class Home extends Component {
   constructor () {
@@ -10,10 +11,17 @@ class Home extends Component {
   }
 
   render () {
+    const border = {
+      margin: '40px'
+    }
     return (
       <section className="home">
         <h1 className="text-center home">WELCOME TO ARTISTKEY</h1>
         <h4 className="text-center sub">Please sign up or sign in</h4>
+        <div style={border} className="text-center">
+          <Button className="justify-content-center" variant="outline-light" href="#sign-up">SIGN UP</Button>
+          <Button className="justify-content-center" variant="outline-light" href="#sign-in">SIGN UP</Button>
+        </div>
       </section>
     )
   }
